a1b2a0928a5f3c1065d7d4e9ff595779b1f32ae7
Sasha <sanaopan4ez@gmail.com>
2020-03-26 00:49:01 +0300

    refactor: delete unessesary code

--- index.js
+++ index.js
@@ -8,69 +8,31 @@ const userName = os.userInfo().username;
 const picturesDir = `C:/Users/${userName}/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets`;
 const finalDest = `C:/Users/${userName}/Pictures`;
 
-const doMagic = async () => {
+new Promise((res, rej) => {
   fs.readdir(picturesDir, (err, files) => {
     if (err) {
-      console.log(err);
-      return;
+      rej(err);
     }
-    const rawPicturesDir = path.join(__dirname, 'raw-pictures');
 
-    fs.mkdir(rawPicturesDir, { recursive: true }, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-    });
-
+    res(files);
+  });
+})
+  .then(files => {
     files.forEach(file => {
       const filePathSrc = path.join(picturesDir, file);
-      const filePathDest = path.join(rawPicturesDir, `${file}.jpg`);
+      const filePathDest = path.join(finalDest, `${file}.jpg`);
+      const { width, height } = imageSize(filePathSrc);
 
-      new Promise((res, rej) => {
+      if (width === 1920 && height === 1080) {
         fs.copyFile(filePathSrc, filePathDest, err => {
           if (err) {
-            rej(err);
+            console.log(err);
             return;
           }
 
-          res(`Copied ${file}`);
+          console.log(`Copied ${file}`);
         });
-      })
-        .then(res => {
-          console.log(res);
-
-          return imageSize(filePathDest);
-        })
-        .then(({ width, height }) => {
-          if (width === 1920 && height === 1080) {
-            const finalDestImg = path.join(finalDest, `${file}.jpg`);
-
-            fs.copyFile(filePathDest, finalDestImg, err => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-
-              console.log(`Copied to final dest ${file}`);
-            });
-          }
-        })
-        .then(() => {
-          fs.unlink(filePathDest, err => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-
-            console.log('File deleted!');
-          });
-        })
-        .catch(err => {
-          reject(new Error(err));
-        });
+      }
     });
-  });
-};
-
-doMagic();
+  })
+  .catch(err => console.log(err));
